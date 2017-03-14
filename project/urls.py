6f98ca81af70e40a8e0bc6c350eecd033d66205d
Your Name <you@example.com>
2017-03-14 19:20:45 +0100

    ejercicio resuelto

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from calc import views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'(-?\d+)([+])(\d+)', views.suma, name="suma"),
+    url(r'(-?\d+)([-])(\d+)', views.resta, name="resta"),
+    url(r'(-?\d+)([*])(\d+)', views.mult, name="mult"),
+    url(r'(-?\d+)([/])(\d+)', views.div, name="div"),
+    url(r'(.*)', views.error, name="error")
+]
