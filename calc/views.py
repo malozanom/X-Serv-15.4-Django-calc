a21a9eda8cc4ae54b2b4b6aafa2a9867cc7a9cfd
Your Name <you@example.com>
2017-03-14 19:34:45 +0100

    cambios menores

--- calc/views.py
+++ calc/views.py
@@ -3,21 +3,26 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def suma(request, op1, op, op2):
     return HttpResponse("<h1>" + op1 + op + op2 + "=" +
                         str(int(op1) + int(op2)) + "</h1>")
+
 
 def resta(request, op1, op, op2):
     return HttpResponse("<h1>" + op1 + op + op2 + "=" +
                         str(int(op1) - int(op2)) + "</h1>")
 
+
 def mult(request, op1, op, op2):
     return HttpResponse("<h1>" + op1 + op + op2 + "=" +
                         str(int(op1) * int(op2)) + "</h1>")
 
+
 def div(request, op1, op, op2):
     return HttpResponse("<h1>" + op1 + op + op2 + "=" +
                         str(int(op1) / int(op2)) + "</h1>")
+
 
 def error(request, parametro):
     return HttpResponse("<h1>Not Found</h1>")
